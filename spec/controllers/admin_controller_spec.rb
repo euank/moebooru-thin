23ef10c96384f073e8d77934c16dca09ff8f0182
Euan <euank@euank.com>
2014-06-03 04:26:10 -0400

    Upgrade assertion from should to expect

--- spec/controllers/admin_controller_spec.rb
+++ spec/controllers/admin_controller_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe AdminController do
+describe AdminController, :type => :controller do
   before(:all) do
     # Note, this must be in this order because the first user gets "autopromoted" to admin
     @admin = FactoryGirl.create(:admin)
@@ -10,52 +10,52 @@ describe AdminController do
   describe "GET index" do
     it "requires you to be logged in" do
       get "index"
-      response.should redirect_to(:controller =>  "user", :action => "login", url: "/admin")
+      expect(response).to redirect_to(:controller =>  "user", :action => "login", url: "/admin")
     end
 
     it "fails if you're not an admin" do
       request.session[:user_id] = @user.id
       get "index"
-      response.should_not be_success
+      expect(response).not_to be_success
     end
     it "works if you're an admin" do
       request.session[:user_id] = @admin.id
       get "index"
-      response.should be_success
+      expect(response).to be_success
     end
   end
   describe "GET edit_user" do
     it "requires you to be logged in" do
       get "edit_user"
-      response.should redirect_to(:controller =>  "user", :action => "login", url: "/admin/edit_user")
+      expect(response).to redirect_to(:controller =>  "user", :action => "login", url: "/admin/edit_user")
     end
 
     it "fails if you're not an admin" do
       request.session[:user_id] = @user.id
       get "edit_user"
-      response.should_not be_success # TODO better error
+      expect(response).not_to be_success # TODO better error
     end
     it "works if you're an admin" do
       request.session[:user_id] = @admin.id
       get "edit_user"
-      response.should be_success
+      expect(response).to be_success
     end
   end
 
   describe "POST edit_user" do
     it "requires you to be logged in" do
       post "edit_user"
-      response.should redirect_to(:controller =>  "user", :action => "login", url: "/admin/edit_user")
+      expect(response).to redirect_to(:controller =>  "user", :action => "login", url: "/admin/edit_user")
     end
     it "fails if you're not an admin" do
       request.session[:user_id] = @user.id
       post "edit_user"
-      response.should_not be_success # TODO better error
+      expect(response).not_to be_success # TODO better error
     end
     it "lets you change a user's level" do
       request.session[:user_id] = @admin.id
       post "edit_user", {user: {name: @user.name, level: @admin.level}}
-      @user.reload.level.should eq @admin.level
+      expect(@user.reload.level).to eq @admin.level
     end
   end
 end
