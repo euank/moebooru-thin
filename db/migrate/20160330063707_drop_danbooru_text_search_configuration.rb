957e703b13939300b9249f83aad236be45461d62
Euan Kemp <euank@euank.com>
2017-05-01 01:32:42 -0700

    migration: move to 'drop if exists'

--- db/migrate/20160330063707_drop_danbooru_text_search_configuration.rb
+++ db/migrate/20160330063707_drop_danbooru_text_search_configuration.rb
@@ -1,7 +1,7 @@
 class DropDanbooruTextSearchConfiguration < ActiveRecord::Migration
   def up
     execute <<-SQL.strip_heredoc
-      DROP TEXT SEARCH CONFIGURATION danbooru;
+      DROP TEXT SEARCH CONFIGURATION IF EXISTS danbooru;
     SQL
   end
 end
